2aefa1853ce34a6bc6795265d7f178f9e4e11fc8
OmarElsayed97 <omar.elsayedmohamed@student.guc.edu.eg>
2020-02-01 10:55:40 +0200

    Adding Teleport and Switching Weapons

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     #region Variables
 
     #region Booleans
-
+    private bool IsTeleporting;
     #endregion
 
     #region Vectors And Transforms
@@ -23,12 +23,28 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] 
     private float f_BlackholeOffset;
+
+    private float f_TeleportOffsetVertical;
+
+    private int i_CurrentWeaponIndex;
     #endregion
 
     #region Strings And Enums
+    enum Weapon
+    {
+        LASER,
+        BLACKHOLE,
+        TELEPORT
+    }
+
+    [SerializeField]
+    Weapon CurrentWeapon;
+
     #endregion
 
     #region Lists
+
+    Weapon[] l_Weapons;
     #endregion
 
     #region Public GameObjects
@@ -64,17 +80,23 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         BulletsPool.InitializePool();
+        l_Weapons = new Weapon[3];
+        l_Weapons[0] = Weapon.LASER;
+        l_Weapons[1] = Weapon.BLACKHOLE;
+        l_Weapons[2] = Weapon.TELEPORT;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Fire();
-        ReleaseBlackHole();
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            StartCoroutine(Dissolve());
-        }
+        if(CurrentWeapon == Weapon.LASER)
+            Fire();
+        if (CurrentWeapon == Weapon.BLACKHOLE)
+            ReleaseBlackHole();
+        if (CurrentWeapon == Weapon.TELEPORT)
+            Teleport();
+
+        SwitchWeapon();
     }
     #endregion
 
@@ -90,7 +112,7 @@ public class PlayerController : MonoBehaviour
 
     void ReleaseBlackHole()
     {
-        if (Input.GetKey(KeyCode.Mouse1))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
             if (!go_InstantiatedBlackHole)
             {
@@ -109,30 +131,97 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    IEnumerator Dissolve()
+    void Teleport()
     {
-        float counter = 1;
-        while (counter > -1)
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            counter -= Time.deltaTime;
-            yield return new WaitForSeconds(0.01f);
-            mat_DissolveMaterial.SetFloat("Vector1_7C06CC4A", counter);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100,1))
+            {
+                Vector3 newPos = new Vector3(hit.point.x, hit.point.y + f_TeleportOffsetVertical, hit.point.z);
+                StartCoroutine(DissolveOut(newPos));
+            }
         }
     }
+
+    void StartTeleporting(Vector3 TeleportPos)
+    {
+        transform.position = TeleportPos;
+        StartCoroutine(DissolveIn());
+    }
+
+    void SwitchWeapon()
+    {
+        bool IsSwitching = false;
+        float wheelValue = Input.GetAxis("Mouse ScrollWheel");
+        if (wheelValue > 0)
+        {
+            if (i_CurrentWeaponIndex == 2)
+                i_CurrentWeaponIndex = 0;
+            else
+                i_CurrentWeaponIndex++;
+
+            IsSwitching = true;
+        }
+        if (wheelValue < 0)
+        {
+            if (i_CurrentWeaponIndex == 0)
+                i_CurrentWeaponIndex = 2;
+            else
+                i_CurrentWeaponIndex--;
+
+            IsSwitching = true;
+        }
+
+
+        if (IsSwitching)
+        {
+            CurrentWeapon = l_Weapons[i_CurrentWeaponIndex];
+            IsSwitching = false;
+        }
+    }
+
+
     #endregion
 
     #region Collisons And Triggers
     #endregion
 
     #region Coroutines
-    
+    IEnumerator DissolveIn()
+    {
+        float counter = 1;
+        while (counter > -1)
+        {
+            counter -= Time.deltaTime;
+            yield return new WaitForSeconds(0.005f);
+            mat_DissolveMaterial.SetFloat("Vector1_7C06CC4A", counter);
+        }
+    }
+
+    IEnumerator DissolveOut(Vector3 TeleportPos)
+    {
+        float counter = 0;
+        while (counter < 1)
+        {
+            counter += Time.deltaTime;
+            yield return new WaitForSeconds(0.005f);
+            mat_DissolveMaterial.SetFloat("Vector1_7C06CC4A", counter);
+        }
+        StartTeleporting(TeleportPos);
+    }
+
+   
+   
+
     #endregion
 
 
 
-   
 
 
-   
-   
+
+
+
 }
