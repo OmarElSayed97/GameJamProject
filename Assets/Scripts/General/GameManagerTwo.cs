45d8510cd7b75fd8e5cc56b1541833eeebed6811
OmarElsayed97 <omar.elsayedmohamed@student.guc.edu.eg>
2020-02-02 14:37:55 +0200

    Updates final

---
+++ Assets/Scripts/General/GameManagerTwo.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerTwo : MonoBehaviour
+{
+
+    #region Singleton
+
+    #endregion
+
+    #region Variables
+
+    #region Booleans
+
+    #endregion
+
+    #region Vectors And Transforms
+
+    #endregion
+
+    #region Integers And Floats
+    #endregion
+
+    #region Strings And Enums
+    #endregion
+
+    #region Lists
+    #endregion
+
+    #region Public GameObjects
+    //[SerializeField] 
+    //private GameObject go_LifeSourceGO;
+    [SerializeField]
+    private GameObject go_GameOverPanel;
+
+    [SerializeField]
+    private GameObject go_Player;
+
+
+    public static int _PlanetsSurvived;
+    public static int _LifeSource;
+    #endregion
+
+    #region Private GameObjects
+    #endregion
+
+    #region UIElements
+
+
+    #endregion
+
+    #region Others
+    GeneralAudioManager AudioManager;
+    #endregion
+
+    #endregion
+
+
+    #region Main Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        AudioManager = GetComponent<GeneralAudioManager>();
+        AudioManager.Play("MainOst");
+        _LifeSource = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!go_Player.gameObject.activeSelf || go_Player.transform.position.y < -50)
+        {
+            go_GameOverPanel.SetActive(true);
+
+        }
+    }
+    #endregion
+
+    #region Methods
+    #endregion
+
+    #region Collisons And Triggers
+    #endregion
+
+    #region Coroutines
+    #endregion
+}
---
+++ Assets/Scripts/UI/HUDManagerTwo.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManagerTwo : MonoBehaviour
+{
+    #region Singleton
+
+    #endregion
+
+    #region Variables
+
+    #region Booleans
+
+    #endregion
+
+    #region Vectors And Transforms
+
+    #endregion
+
+    #region Integers And Floats
+    #endregion
+
+    #region Strings And Enums
+    #endregion
+
+    #region Lists
+    #endregion
+
+    #region Public GameObjects
+    #endregion
+
+    #region Private GameObjects
+    #endregion
+
+    #region UIElements
+
+    [SerializeField]
+    private Sprite[] ui_SpaceShipeInspector;
+
+    [SerializeField]
+    private Sprite[] ui_PlanetSprites;
+
+    [SerializeField]
+    private Image ui_SpaceshipImage;
+
+    [SerializeField]
+    private Image ui_FullTankSprite;
+
+    [SerializeField]
+    private Image ui_PlanetsImg;
+
+    private static Sprite[] ui_SpaceShipSprites;
+    #endregion
+
+    #region Others
+    #endregion
+
+    #endregion
+
+
+    #region Main Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        ui_SpaceShipSprites = new Sprite[4];
+        for (int i = 0; i < ui_SpaceShipeInspector.Length; i++)
+        {
+            ui_SpaceShipSprites[i] = ui_SpaceShipeInspector[i];
+        }
+        ui_SpaceshipImage.sprite = ui_SpaceShipSprites[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateSpaceShipLifeSprite();
+        updatePlanetsSurvivedSprite();
+        FillBar(ui_FullTankSprite, GameManager._LifeSource, 125);
+    }
+    #endregion
+
+    #region Methods
+
+    void updateSpaceShipLifeSprite()
+    {
+        if (PlayerController.i_EnemyHitting == 0)
+        {
+            ui_SpaceshipImage.sprite = ui_SpaceShipSprites[0];
+        }
+        else if (PlayerController.i_EnemyHitting == 1)
+        {
+            ui_SpaceshipImage.sprite = ui_SpaceShipSprites[1];
+        }
+        else if (PlayerController.i_EnemyHitting == 2)
+        {
+            ui_SpaceshipImage.sprite = ui_SpaceShipSprites[2];
+        }
+        else if (PlayerController.i_EnemyHitting == 3)
+        {
+            ui_SpaceshipImage.sprite = ui_SpaceShipSprites[3];
+        }
+    }
+
+    void updatePlanetsSurvivedSprite()
+    {
+        switch (GameManager._PlanetsSurvived)
+        {
+            case 1:
+                ui_PlanetsImg.sprite = ui_PlanetSprites[0];
+                break;
+            case 2:
+                ui_PlanetsImg.sprite = ui_PlanetSprites[1];
+                break;
+            case 3:
+                ui_PlanetsImg.sprite = ui_PlanetSprites[2];
+                break;
+            case 4:
+                ui_PlanetsImg.sprite = ui_PlanetSprites[3];
+                break;
+        }
+    }
+
+    void FillBar(Image img, int value, int originalValue)
+    {
+        float newValue = (float)value / originalValue;
+        img.fillAmount = newValue;
+    }
+
+    #endregion
+
+    #region Collisons And Triggers
+    #endregion
+
+    #region Coroutines
+    #endregion
+}
